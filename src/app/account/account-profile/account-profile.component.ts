369c4f2d3e1a3db9f07f329e74894bbca35c894f
Christian Nuss <christian.nuss@gmail.com>
2021-06-08 11:45:32 -0400

    Rewrite for OpenAPI changes

--- src/app/account/account-profile/account-profile.component.ts
+++ src/app/account/account-profile/account-profile.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Account } from '@app/@shared/interfaces/account';
+import { AccountResponse } from '@app/@openapi/auth';
 import { AuthenticationService } from '@app/@shared/service/authentication.service';
+import { CurrentAccountService } from '@app/@shared/service/current-account.service';
+import { SocialAuthService } from 'angularx-social-login';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-account-profile',
@@ -11,27 +15,25 @@ import { AuthenticationService } from '@app/@shared/service/authentication.servi
 export class AccountProfileComponent implements OnInit {
   formGroup: FormGroup;
 
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private currentAccountService: CurrentAccountService
+  ) {}
 
   ngOnInit(): void {
-    const { payload } = this.authenticationService;
-    if (!payload) {
-      return;
-    }
-
-    const { name, email } = payload;
-    this.formGroup = new FormGroup({
-      name: new FormControl(name, [Validators.required, Validators.minLength(3)]),
-      email: new FormControl(email, [Validators.required, Validators.email]),
-      company: new FormControl('', [Validators.minLength(3)]),
-      updates: new FormControl(true),
+    this.currentAccountService.get().subscribe((account) => {
+      const { name, email, company } = account.detail;
+      this.formGroup = new FormGroup({
+        name: new FormControl(name, [Validators.required, Validators.minLength(3)]),
+        email: new FormControl(email, [Validators.required, Validators.email]),
+        company: new FormControl(company, [Validators.minLength(3)]),
+      });
     });
   }
 
-  onUpdate(account: Account): void {}
+  onUpdate(account: AccountResponse): void {}
 
-  get photoUrl(): string | null {
-    const { payload } = this.authenticationService;
-    return payload ? payload.photoUrl : null;
+  get photoUrl(): string {
+    return this.authenticationService.photoUrl;
   }
 }
